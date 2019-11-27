80363352dd7f6ba4c00df840daf28c2218fe5220
Louis Allard <50803654+Louis-Allard@users.noreply.github.com>
2019-11-27 16:15:25 +0100

    Update fct.js

--- fct.js
+++ fct.js
@@ -1,4 +1,8 @@
- function DownloadJSON2CSV(objArray)
+let json = { "d": "[{\"Id\":1,\"UserName\":\"Sam Smith\"},{\"Id\":2,\"UserName\":\"Fred Frankly\"},{\"Id\":1,\"UserName\":\"Zachary Zupers\"}]" }
+
+DownloadJSON2CSV(json.d); 
+
+function DownloadJSON2CSV(objArray)
     {
         let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
         let str = '';
